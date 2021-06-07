c3edafd71914cdb95bddc82a952f7f694e74cf43
Vladyslav Stusiak <vlstus377@gmail.com>
2021-06-07 11:50:37 +0300

    ua and ru message sources added

--- src/main/java/com/study/taskmanagement/controller/config/WebConfigurer.java
+++ src/main/java/com/study/taskmanagement/controller/config/WebConfigurer.java
@@ -2,9 +2,13 @@ package com.study.taskmanagement.controller.config;
 
 import com.study.taskmanagement.controller.interceptor.UserAuthenticationAddingHandleInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfigurer
@@ -16,7 +20,18 @@ public class WebConfigurer
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationAddingHandleInterceptor);
+        registry.addInterceptor(localeChangeInterceptor());
         WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
     }
 
 }
